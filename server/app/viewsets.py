af9e190053b03f0d87743222389b6f516d5e15b0
Marc Lindenbach <Marc.Lindenbach@Gmail.com>
2016-04-22 15:40:18 -0700

    Set up basic viewsets and url config for REST

---
+++ server/app/viewsets.py
@@ -0,0 +1,13 @@
+from rest_framework import viewsets
+from app.models import DecisionTree, DecisionNode
+from app.serializers import DecisionTreeSerializer, DecisionNodeSerializer
+
+
+class DecisionTreeViewSet(viewsets.ModelViewSet):
+    queryset = DecisionTree.objects.all()
+    serializer_class = DecisionTreeSerializer
+
+
+class DecisionNodeViewSet(viewsets.ModelViewSet):
+    queryset = DecisionNode.objects.all()
+    serializer_class = DecisionNodeSerializer
